4675524f19026492bdbdfad0a54a9bc7f6bedfd2
gdlyeabkov <gdlyeabkov@gmail.com>
2021-10-22 20:45:04 +0300

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="header">
+        <div class="logo">
+            <img width="35px" src="https://yt3.ggpht.com/UhjsV-ihiyEBav8xnrM6Ud2ARTYiiPs8o71LsPDfPunxrtFumlSUc1ydpBice0Z4WOLDcDOboA=s88-c-k-c0x00ffffff-no-rj" alt="">
+        </div>
+        <div class="headerLinks">
+            <span @click="aboutContextMenu = true" >
+                О нас
+            </span>
+            <span @click="aboutContextMenu = true" >
+                Карьера
+            </span>
+            <span @click="aboutContextMenu = true" >
+                Инвесторам
+            </span>
+            <span @click="aboutContextMenu = true" >
+                Наши обязанности
+            </span>
+            <span @click="aboutContextMenu = true" >
+                Новости
+            </span>
+            <span class="material-icons" @click="aboutContextMenu = true" >
+                search
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return {
+            aboutContextMenu: false,
+            carrierContextMenu: false,
+            investorContextMenu: false,
+            commitmentsContextMenu: false,
+            newsroomContextMenu: false,
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+    .header {
+        height: 50px;
+        background-color: rgb(50, 50, 50);
+        box-sizing: border-box;
+        padding: 15px;
+        color: rgb(255, 255, 255);
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .logo {
+        align-self: flex-start;
+        height: 100%;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    .headerLinks {
+        width: 50%;
+        align-self: center;
+        display: flex;
+        align-items: center;
+    }
+
+    .headerLinks > span {
+        cursor: pointer;
+        font-weight: bolder;
+        margin: 0px 15px;
+    }
+
+    .headerLinks > span:hover {
+        text-decoration: underline;
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header />
+    <div class="main">
+      <div class="aboutUs">
+        <div class="aboutUsContent">
+          <span class="aboutUsHeader">
+            О нас
+          </span>
+          <span>
+            Мы создаем самые эпические интерактивные игры и развлечения. Наши платформы погружают игроков в новые, невообразимые миры и предлагают бесчисленные способы игры в них. Мы строим сообщества, которые объединяют людей с целью и причастностью. Мы делаем каждый день веселее с играми от Candy Crush ™, Call of Duty® и World of Warcraft® до Overwatch®, Hearthstone® и Diablo®. Здесь каждый найдет что-то для себя. Чтобы стать героем, нужны герои, и наши сотрудники в Activision, Blizzard Entertainment и King Digital Entertainment - одни из лучших и ярчайших талантов во всех областях технологий, средств массовой информации и развлечений. Тяжелая работа окупается, и мы гордимся тем, что пять лет подряд были названы одной из «100 лучших компаний для работы» по версии журнала FORTUNE.
+          </span>
+          <div class="aboutUsReadMore">
+            <span class="material-icons">
+              help_outline
+            </span>
+            <span>
+              Узнать больше
+            </span>
+          </div>
+        </div>
+        <img src="" alt="">
+      </div>
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
+<style scoped>
+  
+  .aboutUs {
+    display: flex;
+  }
+
+  .aboutUsHeader {
+    text-indent: 25px;
+    font-weight: bolder;
+    font-size: 36px;
+    color: rgb(255, 150, 0);
+  }
+
+  .main {
+    box-sizing: border-box;
+    padding: 25px 35px;
+    background-color: rgb(10, 10, 10);
+    color: rgb(255, 255, 255);
+  }
+
+  .aboutUsContent {
+    width: 65%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .aboutUsReadMore {
+    display: flex;
+    align-items: center;
+  }
+
+  .aboutUsReadMore > span {
+    margin: 5px;
+    cursor: pointer;
+    font-weight: bolder;
+  }
+
+</style>
